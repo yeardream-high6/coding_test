b8e82772656dd5e0dacbf2d63f983f4339bfdade
ms827 <min960827@gmail.com>
2022-05-08 13:00:32 +0900

    백준 단계별로 풀어보기-문자열

--- 이민석/백준/문자열/10809 알파벳찾기.py
+++ 이민석/백준/문자열/10809 알파벳찾기.py
@@ -1,11 +1,5 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-str=list(map(int, input().split()))
-s=[]
-for i in range(ord('a'),ord('z')+1):
-    for j in ord('z')-ord('a')+1:
-        if i==ord(str[j]):
-
 
 
