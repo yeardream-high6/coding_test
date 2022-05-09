646e438ad03db7a5d8496d70c568529c411f75ec
ms827 <min960827@gmail.com>
2022-05-09 19:23:34 +0900

    백준 단계별로 풀어보기-문자열

--- 이민석/백준/문자열/10809 알파벳찾기.py
+++ 이민석/백준/문자열/10809 알파벳찾기.py
@@ -1,5 +1,14 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
+s = input()
+
+abc = list('abcdefghijklmnopqrstuvwxyz')
+
+for i in abc:
+    if i in s:
+        print(s.index(i),end=' ')
+    else:
+        print("-1",end=' ')
 
 
