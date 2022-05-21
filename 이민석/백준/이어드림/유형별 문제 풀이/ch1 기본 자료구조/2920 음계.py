6e237d6072c01420e83124a8b5028053e9d345cc
ms827 <min960827@gmail.com>
2022-05-21 18:16:53 +0900

    ch1 기초

---
+++ 이민석/백준/이어드림/유형별 문제 풀이/ch1 기본 자료구조/2920 음계.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+a = list(map(int, input().split(' ')))
+
+ascending = True
+descending = True
+
+for i in range(1, 8):
+    if a[i] > a[i - 1]:
+        descending = False
+    elif a[i] < a[i - 1]:
+        ascending = False
+
+if ascending:
+    print('ascending')
+elif descending:
+    print('descending')
+else:
+    print('mixed')
