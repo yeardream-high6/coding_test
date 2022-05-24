83383d390e07787c5516e3e06fdefa1016c6fb37
ChocoNaga <duck702702@gmail.com>
2022-05-24 11:16:11 +0900

    추가풀이: 기본 탐색 - 베스트셀러 (딕셔너리 get(key, deault) 사용)

---
+++ 곽치영/백준/나동빈 강사님 - 유형별 문제풀이/06 기본 탐색/06-3 1302 베스트셀러/choconaga1302_short.py
@@ -0,0 +1,6 @@
+import sys
+next(sys.stdin)
+counter = {}
+for name in map(str.strip,sys.stdin):
+    counter[name] = counter.get(name, 1) + 1
+print(min(counter.items(), key=lambda p:(-p[1], p[0]))[0])
