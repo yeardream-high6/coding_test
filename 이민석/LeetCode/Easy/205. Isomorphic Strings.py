6a84bcf7ce9f659fef6ba7e71d60175e5cbd3ad8
ms827 <86936388+ms827@users.noreply.github.com>
2022-08-16 22:43:45 +0900

    205. Isomorphic Strings

---
+++ 이민석/LeetCode/Easy/205. Isomorphic Strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if (len(set(s)) != len(set(t))):
+            return False
+        
+        dic = {}
+        for i in range(len(s)):
+            if s[i] not in dic :
+                dic[(s[i])] = t[i]
+            
+            if dic[(s[i])] != t[i]:
+                return False
+        return True
