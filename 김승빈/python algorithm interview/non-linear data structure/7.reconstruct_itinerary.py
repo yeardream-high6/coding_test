ee1bd94fbc2ad54f1894bf8e2d0dfb6f4c702405
{github KSeungBin} <starsbk7@gmail.com>
2022-09-18 18:09:01 +0900

    [from,to] + lexical order

---
+++ 김승빈/python algorithm interview/non-linear data structure/7.reconstruct_itinerary.py
@@ -0,0 +1,19 @@
+import collections
+from typing import *
+
+
+# method 1 : dfs
+def findItinerary(self, tickets:List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+    for a, b in sorted(tickets):
+        graph[a].append(b)
+    
+    route = []
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop(0))
+        route.append(a)
+    
+    dfs('JFK')
+    return route[::-1]
+    
