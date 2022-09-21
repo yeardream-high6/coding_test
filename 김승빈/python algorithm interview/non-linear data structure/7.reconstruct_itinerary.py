41510d71772839da8308218669c6d02ddd0167cf
{github KSeungBin} <starsbk7@gmail.com>
2022-09-21 21:36:58 +0900

    optimize queue operation using stack

--- 김승빈/python algorithm interview/non-linear data structure/7.reconstruct_itinerary.py
+++ 김승빈/python algorithm interview/non-linear data structure/7.reconstruct_itinerary.py
@@ -16,4 +16,18 @@ def findItinerary(self, tickets:List[List[str]]) -> List[str]:
     
     dfs('JFK')
     return route[::-1]
+
+
+# method 2 : stack
+def findItinerary2(self, tickets:List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+    for a, b in sorted(tickets, reverse=True):
+        graph[a].append(b)
     
+    route, stack = [], ['JFK']
+    while stack:
+        while graph[stack[-1]]:
+            stack.append(graph[stack[-1]].pop(0))
+        route.append(stack.pop())
+    
+    return route[::-1]
