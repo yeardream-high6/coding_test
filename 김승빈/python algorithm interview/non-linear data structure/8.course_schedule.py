abc0ec2e4e070d8e4789fdd381e14477f57e4ce0
{github KSeungBin} <starsbk7@gmail.com>
2022-09-28 21:30:38 +0900

    optimization using pruning

--- 김승빈/python algorithm interview/non-linear data structure/8.course_schedule.py
+++ 김승빈/python algorithm interview/non-linear data structure/8.course_schedule.py
@@ -30,3 +30,40 @@ def canFinish(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
             return False
     
     return True
+
+
+
+# method 2 : optimization using pruning
+def canFinish2(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
+    graph = collections.defaultdict(list)
+    for x, y in prerequisites:
+        graph[x].append(y)
+    
+    traced = set()
+    visited = set()
+
+    def dfs(i):
+        # If it is a cyclic structure, return False
+        if i in traced:
+            return False
+        # Return True if the node has already been visited
+        if i in visited:
+            return True
+        
+        traced.add(i)
+        for y in graph[i]:
+            if not dfs(y):
+                return False
+        # delete circular node after search
+        traced.remove(i)
+        # add visited node after search
+        visited.add(i)
+
+        return True
+    
+    # detect cyclic structure
+    for x in list(graph):
+        if not dfs(x):
+            return False
+    
+    return True
