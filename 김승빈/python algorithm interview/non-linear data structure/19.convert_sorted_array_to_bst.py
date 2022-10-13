fcbe3a15d290b2502465d936b7dd88c6b722cb29
{github KSeungBin} <starsbk7@gmail.com>
2022-10-13 22:23:52 +0900

    height balanced binary search tree

---
+++ 김승빈/python algorithm interview/non-linear data structure/19.convert_sorted_array_to_bst.py
@@ -0,0 +1,19 @@
+from typing import *
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+    if not nums:
+        return None
+    
+    mid = len(nums) // 2
+
+    node = TreeNode(nums[mid])
+    node.left = self.sortedArrayToBST(nums[:mid])
+    node.right = self.sortedArrayToBST(nums[mid+1:])
+
+    return node
