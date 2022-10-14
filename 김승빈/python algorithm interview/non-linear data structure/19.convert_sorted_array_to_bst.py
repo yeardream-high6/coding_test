0463a1d7d651774464de534fd8417a2b79447a56
{github KSeungBin} <starsbk7@gmail.com>
2022-10-14 13:09:25 +0900

    divide-and-conquer algorithm

--- 김승빈/python algorithm interview/non-linear data structure/19.convert_sorted_array_to_bst.py
+++ 김승빈/python algorithm interview/non-linear data structure/19.convert_sorted_array_to_bst.py
@@ -1,3 +1,6 @@
+# Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+# A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
 from typing import *
 
 class TreeNode:
@@ -12,6 +15,7 @@ def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
     
     mid = len(nums) // 2
 
+    # Construct a binary search tree using divide-and-conquer algorithm
     node = TreeNode(nums[mid])
     node.left = self.sortedArrayToBST(nums[:mid])
     node.right = self.sortedArrayToBST(nums[mid+1:])
