f6703ef7eaa2c3216d21f37b4ff683895e0862fd
{github KSeungBin} <starsbk7@gmail.com>
2022-10-22 02:06:42 +0900

    inorder divide-n-conquer with result of preorder

--- 김승빈/python algorithm interview/non-linear data structure/23.build_binary_tree.py
+++ 김승빈/python algorithm interview/non-linear data structure/23.build_binary_tree.py
@@ -1,7 +1,9 @@
 # Construct binary tree from preorder and inorder traversal
+# inorder traversal divide-and-conquer with the result of preorder traversal
 from typing import *
 
-
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, 
+# construct and return the binary tree.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -10,10 +12,13 @@ class TreeNode:
 
 def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
     if inorder:
+        # result of preorder traversal = partitioned index of inorder traversal
+        # Partition the inorder array into Left and Right sub-trees by getting the index  
         index = inorder.index(preorder.pop(0))
 
+        # divide-and-conquer: results of inorder traversal
         node = TreeNode(inorder[index])
         node.left = self.buildTree(preorder, inorder[0:index])
-        node.right = self.buildTree(preorder, inorder[index+1:])
+        node.right = self.buildTree(preorder, inorder[index+1:])    
 
         return node
