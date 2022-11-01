53b4534eda213b66e1819986b6fc2aa12e10c7b5
{github KSeungBin} <starsbk7@gmail.com>
2022-11-01 19:44:48 +0900

    check if a string begins with a specific string

--- 김승빈/python algorithm interview/non-linear data structure/05-trie/26.implement_trie.py
+++ 김승빈/python algorithm interview/non-linear data structure/05-trie/26.implement_trie.py
@@ -1,6 +1,16 @@
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. 
+# There are various applications of this data structure, such as autocomplete and spellchecker.
+
+# Implement the Trie class:
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix, and false otherwise.
+
 import collections
 
-
+# A simple trie implementation using dictionary
+# To implement a trie, we can first create a TrieNode class, which can be used to represent a node in the trie. 
 class TrieNode:
     def __init__(self):
         self.word = False
@@ -10,11 +20,13 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
     
+    # insert a word
     def insert(self, word: str) -> None:
         node = self.root
         for char in word:
             node = node.children(char)
     
+    # check the presence of a word
     def search(self, word: str) -> bool:
         node = self.root
         for char in word:
@@ -24,6 +36,7 @@ class Trie:
         
         return node.word
     
+    # check if a string starts with a particular word
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for char in prefix:
@@ -32,3 +45,9 @@ class Trie:
             node = node.children(char)
         
         return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
