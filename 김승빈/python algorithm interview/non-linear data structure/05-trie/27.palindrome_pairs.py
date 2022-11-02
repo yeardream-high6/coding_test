3c921ef881b6cfcb3d72b84a5451385b9a6eaa2f
{github KSeungBin} <starsbk7@gmail.com>
2022-11-02 21:00:00 +0900

    bruteforce solution for finding palindromic string

---
+++ 김승빈/python algorithm interview/non-linear data structure/05-trie/27.palindrome_pairs.py
@@ -0,0 +1,15 @@
+from typing import *
+
+# method 1: brute force
+def palindromePairs1(self, words: List[str]) -> List[List[str]]:
+    def is_palindrome(word):
+        return word == word[::-1]
+    
+    output = []
+    for i, word1 in enumerate(words):
+        for j, word2 in enumerate(words):
+            if i == j:
+                continue
+            if is_palindrome(word1 + word2):
+                output.append([i,j])
+    return output
