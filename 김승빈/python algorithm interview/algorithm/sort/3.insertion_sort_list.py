4a85bbafd94854144e366a30457804617ce5d6aa
{github KSeungBin} <starsbk7@gmail.com>
2022-11-30 21:08:07 +0900

    fix: add method 2(improving comparison condition)

--- 김승빈/python algorithm interview/algorithm/sort/3.insertion_sort_list.py
+++ 김승빈/python algorithm interview/algorithm/sort/3.insertion_sort_list.py
@@ -3,7 +3,9 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-def insertionSortList(self, head: ListNode) -> ListNode:
+
+# Method 1: Insertion sort
+def insertionSortList1(self, head: ListNode) -> ListNode:
     cur = parent = ListNode(None)
     while head:
         while cur.next and cur.next.val < head.val:
@@ -14,3 +16,17 @@ def insertionSortList(self, head: ListNode) -> ListNode:
         cur = parent
     return cur.next
 
+# Method 2: Improving comparison condition for insertion sort
+def insertionSortList2(self, head: ListNode) -> ListNode:
+    # change the initial value
+    cur = parent = ListNode(0)
+    while head:
+        while cur.next and cur.next.val < head.val:
+            cur = cur.next
+        
+        cur.next, head.next, head = head, cur.next, head.next
+        
+        # the 'cur' pointer goes back only when necessary 
+        if head and cur.val > head.val:
+            cur = parent
+    return parent.next
