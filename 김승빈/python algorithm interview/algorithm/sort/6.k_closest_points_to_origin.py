d577dbd5780651e78362de4a810a4c7ea6699e05
{github KSeungBin} <starsbk7@gmail.com>
2022-12-03 19:23:59 +0900

    feat: euclidean distance

---
+++ 김승빈/python algorithm interview/algorithm/sort/6.k_closest_points_to_origin.py
@@ -0,0 +1,13 @@
+from typing import *
+import heapq
+def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+    heap = []
+    for (x, y) in points:
+        dist = x ** 2 + y ** 2
+        heapq.heappush(heap, (dist, x, y))
+    
+    result = []
+    for _ in range(K):
+        (dist, x, y) = heapq.heappop(heap)
+        result.append((x,y))
+    return result
