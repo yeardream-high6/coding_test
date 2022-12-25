f8b2a9ce9473ab4422c6ca79c3f1c2c007bc2b97
{github KSeungBin} <starsbk7@gmail.com>
2022-12-25 19:46:46 +0900

    fix: add method2(binary search)

--- 김승빈/python algorithm interview/algorithm/binary search/3.intersection_of_two_arrays.py
+++ 김승빈/python algorithm interview/algorithm/binary search/3.intersection_of_two_arrays.py
@@ -10,3 +10,16 @@ def intersection1(self, nums1: List[int], nums2: List[int]) -> List[int]:
                 result.add(n1)
     
     return result
+
+
+# method 2: binary search
+import bisect
+def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    result: Set = set()
+    nums2.sort()
+    for n1 in nums1:
+        i2 = bisect.bisect_left(nums2, n1)
+        if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
+            result.add(n1)
+    
+    return result
