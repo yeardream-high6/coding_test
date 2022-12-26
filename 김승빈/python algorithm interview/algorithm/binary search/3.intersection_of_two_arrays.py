1340932a3aa7865b862ca8cadfd8405f3364895f
{github KSeungBin} <starsbk7@gmail.com>
2022-12-26 22:56:48 +0900

    fix: add method3(two-pointer)

--- 김승빈/python algorithm interview/algorithm/binary search/3.intersection_of_two_arrays.py
+++ 김승빈/python algorithm interview/algorithm/binary search/3.intersection_of_two_arrays.py
@@ -23,3 +23,23 @@ def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
             result.add(n1)
     
     return result
+
+
+# method 3: two-pointer
+def intersection3(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    result: Set = set()
+    nums1.sort()
+    nums2.sort()
+    i = j = 0
+
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] > nums2[j]:
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        else:
+            result.add(nums1[i])
+            i += 1
+            j += 1
+    
+    return result
