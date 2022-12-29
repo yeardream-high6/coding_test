497a17631d3a486db62d605b6568fe8fa8ecfc7b
{github KSeungBin} <starsbk7@gmail.com>
2022-12-30 00:09:37 +0900

    fix: add method3(bisect module + slicing)

--- 김승빈/python algorithm interview/algorithm/binary search/4.two_sum_ii.py
+++ 김승빈/python algorithm interview/algorithm/binary search/4.two_sum_ii.py
@@ -1,5 +1,5 @@
 from typing import *
-
+import bisect
 
 # method 1: two-pointer
 def twoSum(self, numbers: List[int], target: int) -> List[int]:
@@ -27,3 +27,12 @@ def twoSum2(self, numbers: List[int], target: int) -> List[int]:
                 right = mid - 1
             else:
                 return k + 1, mid + 1
+
+
+# method 3: bisect module + slicing
+def twoSum3(self, numbers: List[int], target: int) -> List[int]:
+    for k, v in enumerate(numbers):
+        expected = target - v
+        i = bisect.bisect_left(numbers[k + 1:], expected)
+        if i < len(numbers[k + 1:]) and numbers[i + k + 1] == expected:
+            return k + 1, i + k + 2
