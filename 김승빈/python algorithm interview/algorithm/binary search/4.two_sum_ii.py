c7bb3ebed8e9adcf14556e103784e61c6a3994ec
{github KSeungBin} <starsbk7@gmail.com>
2022-12-29 22:36:30 +0900

    fix: add method2(binary search)

--- 김승빈/python algorithm interview/algorithm/binary search/4.two_sum_ii.py
+++ 김승빈/python algorithm interview/algorithm/binary search/4.two_sum_ii.py
@@ -11,3 +11,19 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
             right -= 1
         else:
             return left + 1, right + 1
+
+
+# method 2: binary search
+def twoSum2(self, numbers: List[int], target: int) -> List[int]:
+    for k, v in enumerate(numbers):
+        left, right = k + 1, len(numbers) - 1
+        expected = target - v
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if numbers[mid] < expected:
+                left = mid + 1
+            elif numbers[mid] > expected:
+                right = mid - 1
+            else:
+                return k + 1, mid + 1
