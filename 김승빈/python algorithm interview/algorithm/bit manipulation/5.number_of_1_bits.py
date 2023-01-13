3cfa2a13c8f3ef506aadb5f0aa75df18dfde43a9
{github KSeungBin} <starsbk7@gmail.com>
2023-01-13 23:37:42 +0900

    fix: add method2(bitwise operation)

--- 김승빈/python algorithm interview/algorithm/bit manipulation/5.number_of_1_bits.py
+++ 김승빈/python algorithm interview/algorithm/bit manipulation/5.number_of_1_bits.py
@@ -1,3 +1,12 @@
 # method 1: count the number of 1's
 def hammingWeight(self, n: int) -> int:
     return bin(n).count('1')
+
+
+# method 2: bitwise operation
+def hammingWeight2(self, n: int) -> int:
+    count = 0
+    while n:
+        n &= n - 1
+        count += 1
+    return count
