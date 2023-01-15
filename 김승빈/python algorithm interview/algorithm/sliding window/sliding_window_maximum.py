74213b83eff585bac9462424f852536776625faf
{github KSeungBin} <starsbk7@gmail.com>
2023-01-15 22:53:38 +0900

    fix: add method2(queuing optimization)

--- 김승빈/python algorithm interview/algorithm/sliding window/sliding_window_maximum.py
+++ 김승빈/python algorithm interview/algorithm/sliding window/sliding_window_maximum.py
@@ -12,3 +12,25 @@ def maxSlidingWindow1(self, nums: List[int], k: int) -> List[int]:
     
     return r
 
+
+# method 2: queuing optimization
+import collections
+def maxSlidingWindow2(self, nums: List[int], k: int) -> List[int]:
+    results = []
+    window = collections.deque()
+    current_max = float('-inf')
+    for i, v in enumerate(nums):
+        window.append(v)
+        if i < k - 1:
+            continue
+
+        if current_max == float('inf'):
+            current_max = max(window)
+        elif v > current_max:
+            current_max = v
+        
+        results.append(current_max)
+
+        if current_max == window.popleft():
+            current_max = float('-inf')
+    return results
