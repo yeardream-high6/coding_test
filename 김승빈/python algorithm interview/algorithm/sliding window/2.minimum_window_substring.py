8ad4a423beb526354cbfb7f472dea81ddd9bcb35
{github KSeungBin} <starsbk7@gmail.com>
2023-01-17 23:24:24 +0900

    fix: add method2(two-pointer)

--- 김승빈/python algorithm interview/algorithm/sliding window/2.minimum_window_substring.py
+++ 김승빈/python algorithm interview/algorithm/sliding window/2.minimum_window_substring.py
@@ -22,3 +22,28 @@ def minWindow1(self, s: str, t: str) -> str:
             if contains(list(s_substr), list(t)):
                 return s_substr
     return ''
+
+
+# method 2: two-pointer
+import collections
+def minWindow2(self, s: str, t: str) -> str:
+    need = collections.Counter(t)
+    missing = len(t)
+    left = start = end = 0
+
+    # move right pointer
+    for right, char in enumerate(s, 1):
+        missing -= need[char] > 0
+        need[char] -= 1
+
+        if missing == 0:
+            while left < right and need[s[left]] < 0:
+                need[s[left]] += 1
+                left += 1
+            
+            if not end or right - left <= end - start:
+                start, end = left, right
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+    return s[start:end]
