9a0df9ddc764bf1d61ce81a26c1ad431b9557067
{github KSeungBin} <starsbk7@gmail.com>
2023-01-22 22:53:51 +0900

    feat: best time to buy and sell stock

---
+++ 김승빈/python algorithm interview/algorithm/greedy/3.stock.py
@@ -0,0 +1,11 @@
+from typing import *
+
+
+# method 1: greedy algorithm
+def maxProfit(self, prices: List[int]) -> int:
+    result = 0
+
+    for i in range(len(prices) - 1):
+        if prices[i + 1] > prices[i]:
+            result += prices[i + 1] - prices[i]
+    return result
