96d1de2b74074deea4c472cc6347d3e374d8b20e
{github KSeungBin} <starsbk7@gmail.com>
2023-02-09 18:38:46 +0900

    fix: add method2(binary search)

--- 김승빈/python algorithm interview/algorithm/greedy/7.assign_cookies.py
+++ 김승빈/python algorithm interview/algorithm/greedy/7.assign_cookies.py
@@ -14,3 +14,17 @@ def findContentChildren1(self, g: List[int], s: List[int]) -> int:
         cookie_j += 1
     
     return child_i
+
+
+# method 2: binary search
+def findContentChildren2(self, g: List[int], s: List[int]) -> int:
+    g.sort()
+    s.sort()
+
+    result = 0
+    for i in s:
+        # search larger index
+        index = bisect.bisect_right(g, i)
+        if index > result:
+            result += 1
+    return result
