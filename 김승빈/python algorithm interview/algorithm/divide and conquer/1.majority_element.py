231c65f914c587892adbb3def1d8f07fdcab7fb3
{github KSeungBin} <starsbk7@gmail.com>
2023-02-21 17:34:23 +0900

    fix: add method2(dynamic programming)

--- 김승빈/python algorithm interview/algorithm/divide and conquer/1.majority_element.py
+++ 김승빈/python algorithm interview/algorithm/divide and conquer/1.majority_element.py
@@ -1,8 +1,19 @@
 from typing import *
-
+import collections
 
 # method 1: brute-force
-def majorityElement(self, nums: List[int]) -> int:
+def majorityElement1(self, nums: List[int]) -> int:
     for num in nums:
         if nums.count(num) > len(nums) // 2:
+            return num
+
+
+# method 2: dynamic programming
+def majorityElement2(self, nums: List[int]) -> int:
+    counts = collections.defaultdict(int)
+    for num in nums:
+        if counts[num] == 0:
+            counts[num] = nums.count(num)
+        
+        if counts[num] > len(nums) // 2:
             return num
