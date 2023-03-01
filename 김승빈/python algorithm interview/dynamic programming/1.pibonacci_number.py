9b670b51cf27576496d5c7dd4dca3b83c313aa5e
{github KSeungBin} <starsbk7@gmail.com>
2023-03-01 21:41:43 +0900

    fix: add method2(memoization)

--- 김승빈/python algorithm interview/dynamic programming/1.pibonacci_number.py
+++ 김승빈/python algorithm interview/dynamic programming/1.pibonacci_number.py
@@ -3,3 +3,18 @@ def fib(self, N: int) -> int:
     if N <= 1:
         return N
     return self.fib(N - 1) + self.fib(N - 2)
+
+
+# method 2: memoization
+import collections
+class Solution:
+    dp = collections.defaultdict(int)
+
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        
+        if self.dp[N]:
+            return self.dp[N]
+        self.dp[N] = self.fib(N - 1) + self.fib(N - 2)
+        return self.dp[N]
