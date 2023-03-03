7ac2178498c27d5597170bb0a4bbb1efa4fdd1e4
{github KSeungBin} <starsbk7@gmail.com>
2023-03-03 19:57:34 +0900

    fix: add method3(tabulation)

--- 김승빈/python algorithm interview/dynamic programming/1.pibonacci_number.py
+++ 김승빈/python algorithm interview/dynamic programming/1.pibonacci_number.py
@@ -18,3 +18,16 @@ class Solution:
             return self.dp[N]
         self.dp[N] = self.fib(N - 1) + self.fib(N - 2)
         return self.dp[N]
+
+
+# method 3: tabulation
+class Solution:
+    dp = collections.defaultdict(int)
+
+    def fib(self, N: int) -> int:
+        self.dp[0] = 0
+        self.dp[1] = 1
+
+        for i in range(2, N + 1):
+            self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
+        return self.dp[N]
